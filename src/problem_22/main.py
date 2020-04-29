3aa5b283f64cf8638b4b87623a08287ae3058bbf
kPavel0206 <paulkassianov@gmail.com>
2020-04-29 15:35:45 +0300

    add problem_22 solution

---
+++ src/problem_22/main.py
@@ -0,0 +1,31 @@
+import string
+
+
+def char_position(letter):
+    return string.ascii_lowercase.index(letter.lower()) + 1
+
+
+def name_score(name: str, index: int) -> int:
+    sum_ = sum((char_position(x) for x in name if x.isalpha()))
+    return sum_ * index
+
+
+def main():
+    with open("names.txt", "r") as f:
+        src_names = f.readlines()[0].split(",")
+
+    sorted_names = sorted(src_names)
+
+    index = 1
+    score_sum = 0
+    for name in sorted_names:
+        score = name_score(name, index)
+        score_sum += score
+        index += 1
+
+    print(f"result_score: {score_sum}")
+    print("exit")
+
+
+if __name__ == '__main__':
+    main()
