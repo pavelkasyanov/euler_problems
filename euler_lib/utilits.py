3c13e27a8776d1387701fd7466f689467dd5f430
kPavel0206 <paulkassianov@gmail.com>
2020-04-30 16:43:12 +0300

    add task_37

--- euler_lib/utilits.py
+++ euler_lib/utilits.py
@@ -1,4 +1,47 @@
+import array
 
 
 def is_palindrome(s: str) -> bool:
     return s == s[::-1]
+
+
+def sqrt(x):
+    assert x >= 0
+    i = 1
+    while i * i <= x:
+        i *= 2
+    y = 0
+    while i > 0:
+        if (y + i) ** 2 <= x:
+            y += i
+        i //= 2
+    return y
+
+
+def is_prime(x):
+    if x <= 1:
+        return False
+    elif x <= 3:
+        return True
+    elif x % 2 == 0:
+        return False
+    else:
+        for i in range(3, sqrt(x) + 1, 2):
+            if x % i == 0:
+                return False
+        return True
+
+
+def prime_generator(limit):
+    if limit >= 2:
+        yield 2
+
+    isprime = array.array("B", b"\x01" * ((limit - 1) // 2))
+    sieveend = sqrt(limit)
+    for i in range(len(isprime)):
+        if isprime[i] == 1:
+            p = i * 2 + 3
+            yield p
+            if i <= sieveend:
+                for j in range((p * p - 3) >> 1, len(isprime), p):
+                    isprime[j] = 0
---
+++ src/problem_37/main.py
@@ -0,0 +1,43 @@
+import time
+
+from euler_lib.utilits import prime_generator, is_prime
+
+
+def is_truncatable(n: str)-> bool:
+    n = str(n)
+
+    for i in range(1, len(n)):
+        t = int(n[i:])
+        if not is_prime(t):
+            return False
+
+        t = int(n[:i])
+        if not is_prime(t):
+            return False
+
+    return True
+
+def main():
+    print("start")
+    start = time.clock()
+
+    res_list = []
+
+    for prime in prime_generator(100000000):
+        print(prime)
+
+        if prime < 20:
+            continue
+
+        if is_truncatable(prime):
+            res_list.append(prime)
+
+        if len(res_list) >= 11:
+            break
+
+    print(f"result: {sum(res_list)}")
+    print(f"exit, time: {time.clock() - start}")
+
+
+if __name__ == '__main__':
+    main()
