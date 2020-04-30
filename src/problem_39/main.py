ce1d5a7b890c2e22958939d1bcf995698484ff94
kPavel0206 <paulkassianov@gmail.com>
2020-04-30 17:18:26 +0300

    add task 39

---
+++ src/problem_39/main.py
@@ -0,0 +1,26 @@
+import time
+
+
+def count_solutions(p):
+    result = 0
+    for a in range(1, p + 1):
+        for b in range(a, (p - a) // 2 + 1):
+            c = p - a - b
+            if a * a + b * b == c * c:
+                result += 1
+    return result
+
+
+def main():
+    print("start")
+    start = time.clock()
+
+    result = max(range(1, 1001), key=count_solutions)
+
+    print(f"result: {result}")
+
+    print(f"exit, time: {time.clock() - start}")
+
+
+if __name__ == '__main__':
+    main()
